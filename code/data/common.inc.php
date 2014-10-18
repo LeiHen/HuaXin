203abc506121c7afc0f4af89124d63f47de965e7
流星的泪痕 <61955365@qq.com>
2014-10-18 19:52:27 +0800

    add cms

---
+++ code/data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'dede_huaxin';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'root';
+$cfg_dbprefix = 'hx_';
+$cfg_db_language = 'utf8';
+
+
+?>
---
+++ code/data/module/modulescache.php
@@ -0,0 +1,5 @@
+<?php
+global $allmodules;
+$GLOBALS['allmodules']['1f35620fb42d452fa2bdc1dee1690f92']='1f35620fb42d452fa2bdc1dee1690f92.xml';
+$GLOBALS['allmodules']['b437d85a7a7bc778c9c79b5ec36ab9aa']='b437d85a7a7bc778c9c79b5ec36ab9aa.xml';
+?>
